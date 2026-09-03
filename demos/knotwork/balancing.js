4eb744d5353fa2aa84146df82062f1ce29f18cf5
Daniel Bergey <bergey@teallabs.org>
2026-09-03 19:29:51 -0400

    knotwork: buttons

--- demos/knotwork/balancing.js
+++ demos/knotwork/balancing.js
@@ -1,3 +1,8 @@
+function distance_squared(a, b) {
+    // TODO handle pt-line
+    return (a.x - b.x)**2 + (a.y - b.y)**2;
+}
+
 function polar_pt(angle, radius) {
     return {
         x: radius * Math.cos(angle * 2 * Math.PI),
@@ -139,6 +144,34 @@ function rotate(shape, angle) {
     }
 }
 
+function parallel_through_pt(line, point) {
+    return {
+        p: point,
+        q: {
+            x: line.q.x - line.p.x + point.x,
+            y: line.q.y - line.p.y + point.y,
+        }
+    };
+}
+
+function trim_extend(line, to) {
+    const ix = intersect_lines(line, to);
+    if (ix === null) {
+        return line;
+    }
+    if (distance_squared(ix, line.p) < distance_squared(ix, line.q)) {
+        return {
+            p: ix,
+            q: line.q
+        }
+    } else {
+        return {
+            p: line.p,
+            q: ix,
+        }
+    }
+}
+
 function repeat_rotate(shapes, copies) {
     let ret = [];
     for (let i=0; i< copies; i++) {
@@ -152,6 +185,23 @@ function render_grid(shapes, offset, copies) {
         for (let j=Math.floor(-copies/2); j<copies/2; j++) {
             ctx.save();
             ctx.translate(i * offset, j * offset);
+            render(shapes);
+            ctx.restore();
+        }
+    }
+}
+
+function render_hex_grid(shapes, side, copies) {
+    const p = polar_pt(1/12, side);
+    const x_offset = p.x;
+    const y_offset = side + p.y;
+    for (let i = Math.floor(-copies/2); i<copies/2; i++) {
+        for (let j=Math.floor(-copies/2); j<copies/2; j++) {
+            ctx.save();
+            ctx.translate(i * 2 * x_offset, j*y_offset);
+            if (j % 2 != 0) {
+                ctx.translate(x_offset, 0);
+            }
             render(shapes);
             ctx.restore();
         }
@@ -181,47 +231,79 @@ function render(shape) {
     ctx.stroke();
 }
 
-// ctx.lineWidth = 3;
-ctx.strokeStyle = "white";
+ctx.strokeStyle = "green";
 const r = 100;
 
 // construction marks for debugging
 // render({c: {x: 0, y: 0}, r});
 // render([{p: origin, q: {x: r, y: 0}}, {p: {x: r, y: 0}, q: {x: r, y: r}}, {p: {x: r, y: r}, q: {x: 0, y: r}}, {p: origin, q: {x: 0, y: r}}]);
 
-let segments = [];
 // Eric Broug, pages 27-29
-
-// segments.push(fit_y({p: polar_pt(3/4, r), q: polar_pt(1/8, r)}, 0, r));
-// const right = polar_pt(0, r);
-// const p = intersect_lines({p: origin, q: {x: 100, y: 100}}, {p: polar_pt(3/8, r), q: right});
-// segments.push({p, q: right});
-// const refl = [...segments, ...reflect_xy(segments)];
-// const square = repeat_rotate([...segments, ...reflect_xy(segments)], 4);
+function page_27() {
+    let segments = [];
+    segments.push(fit_y({p: polar_pt(3/4, r), q: polar_pt(1/8, r)}, 0, r));
+    const right = polar_pt(0, r);
+    const p = intersect_lines({p: origin, q: {x: 100, y: 100}}, {p: polar_pt(3/8, r), q: right});
+    segments.push({p, q: right});
+    const refl = [...segments, ...reflect_xy(segments)];
+    const square = repeat_rotate([...segments, ...reflect_xy(segments)], 4);
+    render_grid_blue_white(square);
+}
 
 // pages 30-33
 // 4 segments, each repeated in 8-fold symmetry (or 2x8 + 1x16)
 // 2 segments of the octogon, one where circle touches square, one extended past circle
 // two segments near radii of the circle
 // the offset from radii found by intersection of squares
+function page_30() {
+    let segments = [];
+    const octo = {p: polar_pt(0, r), q: polar_pt(1/8, r)};
+    const X = intersect_lines({p: polar_pt(-1/8, r), q: polar_pt(1/8, r)}, {p: polar_pt(0, r), q: polar_pt(1/4, r)});
+    const N = {p: X, q: rotate(X, 3/8)};
+    const SE = {p: X, q: rotate(X, 5/8)};
+    // render(SE)
+    // render(N)
+    segments.push(
+        {p: intersect_lines(N, octo), q: octo.p},
+        {p: intersect_lines(SE, octo), q: fit_y(octo, 0, r).q}
+    );
+    let NE = rotate(SE, 1/4);
+    segments.push({p: segments[0].p, q: intersect_lines(N, NE)}); // a segment of N
+    let E = rotate(N, -1/4);
+    segments.push({p: segments[1].p, q: intersect_lines(SE, E)}); // a segment of SE
+    const square = repeat_rotate([...segments, ...reflect_xy(segments)], 4);
+    render_grid_blue_white(square);
+}
 
-const octo = {p: polar_pt(0, r), q: polar_pt(1/8, r)};
-const X = intersect_lines({p: polar_pt(-1/8, r), q: polar_pt(1/8, r)}, {p: polar_pt(0, r), q: polar_pt(1/4, r)});
-const N = {p: X, q: rotate(X, 3/8)};
-const SE = {p: X, q: rotate(X, 5/8)};
-// render(SE)
-// render(N)
-segments.push(
-    {p: intersect_lines(N, octo), q: octo.p},
-    {p: intersect_lines(SE, octo), q: fit_y(octo, 0, r).q}
-);
-let NE = rotate(SE, 1/4);
-segments.push({p: segments[0].p, q: intersect_lines(N, NE)}); // a segment of N
-let E = rotate(N, -1/4);
-segments.push({p: segments[1].p, q: intersect_lines(SE, E)}); // a segment of SE
-const square = repeat_rotate([...segments, ...reflect_xy(segments)], 4);
+function page_34() {
+    const d1 = polar_pt(-1/12, r);
+    const d2 = polar_pt(-3/12, r);
+    const u1 = polar_pt(1/12, r);
+    const u2 = polar_pt(3/12, r);
+    const ix = intersect_lines({p: d2, q: u1}, {p: d1, q: u2});
+    const line = trim_extend(trim_extend(parallel_through_pt({p: d1, q: u1}, ix), {p: u1, q: u2}), {p: d2, q: d1});
+    const hex =  repeat_rotate([line], 6);
+    render_hex_blue_white(hex);
+}
 
-render_grid(square, 2 * r, Math.ceil(canvas.width / (2*r))+1);
-ctx.strokeStyle = "blue";
-render(square);
-console.log('done');
+function render_grid_blue_white(square) {
+    ctx.clearRect(-1/2 * canvas.width, -1/2 * canvas.height, canvas.width, canvas.height);
+    ctx.strokeStyle = "white";
+    render_grid(square, 2 * r, Math.ceil(canvas.width / (2*r))+1);
+    ctx.strokeStyle = "blue";
+    render(square);
+    console.log('done');
+}
+
+function render_hex_blue_white(hex) {
+    ctx.clearRect(-1/2 * canvas.width, -1/2 * canvas.height, canvas.width, canvas.height);
+    ctx.strokeStyle = "white";
+    render_hex_grid(hex, r, Math.ceil(canvas.width / (2*r)) + 5);
+    ctx.strokeStyle = "blue";
+    render(hex);
+    console.log('done');
+}
+
+document.getElementById("27").onclick = page_27;
+document.getElementById("30").onclick = page_30;
+document.getElementById("34").onclick = page_34;
